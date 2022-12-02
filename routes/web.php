b85ddaf819ead8e0ba9da2d4adf240a61fe98b60
Why-sptr <wahyucahyos380@gmail.com>
2022-12-02 09:00:30 +0700

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 //Dashboard
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('admin/dashboard');
 });
 
